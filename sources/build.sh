9c33be068697309a4d93bebe2b77e1a9189c54ad
kosbarts <kosbarts@yahoo.com>
2020-01-17 09:33:10 +0100

    Changed Build and GlyphsStatic file

--- sources/build.sh
+++ sources/build.sh
@@ -2,9 +2,9 @@
 #source ../venv/bin/activate
 set -e
 
-#echo "Generating Static fonts"
-#mkdir -p ../fonts/static/ttfs
-#fontmake -g Commissioner-Variable.glyphs -i -a -o ttf --output-dir ../fonts/static/ttfs/
+echo "Generating Static fonts"
+mkdir -p ../fonts/static/ttfs
+fontmake -g Commissioner-Variable.glyphs -i -a -o ttf --output-dir ../fonts/static/ttfs/
 
 #echo "Generating VFs"
 #mkdir -p ../fonts/variable
@@ -29,7 +29,7 @@ rm ../fonts/static/ttfs/*backup*.ttf
 #do
 #	gftools fix-dsig -f $vf;
 #	gftools fix-nonhinting $vf "$vf.fix";
-#	gftools fix-hinting $vf "$vf.fix";
+#	gftools fix-hinting $vf;
 #	mv "$vf.fix" $vf;
 #	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
 #	rtrip=$(basename -s .ttf $vf)
@@ -49,22 +49,5 @@ rm ../fonts/static/ttfs/*backup*.ttf
 #fontbakery check-googlefonts ../fonts/variable/*.ttf --ghmarkdown ../checks/checks_variable.md
 fontbakery check-googlefonts ../fonts/static/ttfs/*.ttf --ghmarkdown ../checks/checks_static.md
 
-#cd ..
-##
-### ============================================================================
-### Autohinting ================================================================
-##
-#statics=$(ls fonts/static/ttfs/*.ttf)
-#echo hello
-#for file in $statics; do 
-#    echo "fix DSIG in " ${file}
-#    gftools fix-dsig --autofix ${file}
-#
-#    echo "TTFautohint " ${file}
-#    # autohint with detailed info
-#    hintedFile=${file/".ttf"/"-hinted.ttf"}
-#    ttfautohint -I ${file} ${hintedFile} 
-#    cp ${hintedFile} ${file}
-#    rm -rf ${hintedFile}
-#done
+
 
