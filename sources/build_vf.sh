d2336c70add33f8eb2eb7b7b9b680d66a961c2e3
Yanone <post@yanone.de>
2020-07-21 12:08:24 +0200

    Remove unwanted fvar instances

--- sources/build_vf.sh
+++ sources/build_vf.sh
@@ -51,6 +51,9 @@ do
 	# patch Name and STAT table #	
 	ttx -m $vf "../sources/helpers/vf-patch.ttx"
 	mv "../sources/helpers/vf-patch.ttf" "../fonts/variable/Commissioner[FLAR,VOLM,slnt,wght].ttf"
+
+	# remove unwanted instances
+	python ../sources/helpers/removeUnwantedVFInstances.py $vf
 done
 # remove any backup files #
 rm ../fonts/variable/*backup*.ttf
